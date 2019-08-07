d85c10758ca7ef8ed36f7442900f2b88fdf09138
kmucrc <42525811+kmucrc@users.noreply.github.com>
2019-08-07 15:48:57 +0900

    create new repository
    create blue repository

---
+++ app/src/main/java/com/crc/blue/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.crc.blue.splash
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
